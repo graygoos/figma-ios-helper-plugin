e08c2e7ec299740e3cb7977a21ce1d379080bdf6
Femi Aliu <52054174+graygoos@users.noreply.github.com>
2024-08-03 00:17:05 +0100

    Update code.js
    added more detailed logging

--- code.js
+++ code.js
@@ -1,19 +1,35 @@
 figma.showUI(__html__);
 
 figma.ui.onmessage = (msg) => {
-  if (msg.type === 'delete') {
-    for (const node of figma.currentPage.selection) {
-      node.remove();
+  console.log("Plugin received message:", msg);
+  try {
+    switch (msg.type) {
+      case 'delete':
+        if (figma.currentPage.selection.length > 0) {
+          figma.currentPage.selection.forEach(node => node.remove());
+          figma.ui.postMessage({type: 'success', action: 'delete'});
+        } else {
+          figma.ui.postMessage({type: 'error', action: 'delete', message: 'No selection'});
+        }
+        break;
+      case 'copy':
+        figma.copy(figma.currentPage.selection);
+        figma.ui.postMessage({type: 'success', action: 'copy'});
+        break;
+      case 'paste':
+        figma.paste();
+        figma.ui.postMessage({type: 'success', action: 'paste'});
+        break;
+      case 'duplicate':
+        const duplicated = figma.duplicate(figma.currentPage.selection);
+        figma.currentPage.selection = duplicated;
+        figma.ui.postMessage({type: 'success', action: 'duplicate'});
+        break;
+      default:
+        figma.ui.postMessage({type: 'error', action: msg.type, message: 'Unknown action'});
     }
-  } else if (msg.type === 'duplicate') {
-    for (const node of figma.currentPage.selection) {
-      const clone = node.clone();
-      figma.currentPage.appendChild(clone);
-    }
-  } else if (msg.type === 'copy') {
-    figma.copy(figma.currentPage.selection);
-  } else if (msg.type === 'paste') {
-    figma.paste();
+  } catch (error) {
+    console.error("Error in plugin:", error);
+    figma.ui.postMessage({type: 'error', action: msg.type, message: error.message});
   }
-  // Add more actions as needed
 }
